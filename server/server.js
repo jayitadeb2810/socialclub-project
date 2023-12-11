c3dea8c7a25b92fa3ef3574d067e26cc1b3b14cb
jayitadeb2810 <jayitadeb2810@gmail.com>
2023-12-11 11:29:37 +0530

    Third Commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const server = http.createServer(app)
 // export const io = new Server(server)
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://socialclub-app.onrender.com",
+    ],
   },
 })
 
