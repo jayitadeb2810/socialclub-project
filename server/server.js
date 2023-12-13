2393c863aef8cd77fe746ecb035aeb35b43007fa
jayitadeb2810 <jayitadeb2810@gmail.com>
2023-12-13 16:35:47 +0530

    Nineth Commit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ export const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://socialclub-app.onrender.com",
+      "https://socialclub-poject.onrender.com/",
     ],
   },
 })
