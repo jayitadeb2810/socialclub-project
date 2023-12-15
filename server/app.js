9bda8c52db67ec02453d193b07aaf5052ba04aa4
jayitadeb2810 <jayitadeb2810@gmail.com>
2023-12-15 15:57:52 +0530

    new commit

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const app = express()
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://socialclub-application.onrender.com/"
+    "https://socialclub-application.onrender.com"
   )
   res.setHeader(
     "Access-Control-Allow-Methods",
